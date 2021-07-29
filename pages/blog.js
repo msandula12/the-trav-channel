50d4f094c88eb7e1eb31d4d799d73f03a3eaf496
Mike Sandula <msandula@gmail.com>
2021-07-29 09:23:06 -0400

    feat: add Head component to pages

--- pages/blog.js
+++ pages/blog.js
@@ -1,5 +1,16 @@
+import Head from 'next/head';
+
 const Blog = () => {
-  return <div>Coming soon...</div>;
+  return (
+    <div>
+      <Head>
+        <title>Blog - The Trav Channel</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>Coming soon...</main>
+    </div>
+  );
 };
 
 export default Blog;
--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,16 @@
+import Head from 'next/head';
+
 const Contact = () => {
-  return <div>Coming soon...</div>;
+  return (
+    <div>
+      <Head>
+        <title>Contact - The Trav Channel</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>Coming soon...</main>
+    </div>
+  );
 };
 
 export default Contact;
