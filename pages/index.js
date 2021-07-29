89a3025457d5d217000d564faa127937b27aa7b9
Mike Sandula <msandula@gmail.com>
2021-07-29 15:15:22 -0400

    styling: remove duplicate class

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import styles from '../styles/Home.module.css';
 
 const Home = () => {
   return (
-    <main className={styles.home}>
+    <main className="centered">
       <div className={`${styles.logo} zoom-in`}>
         <div className={styles.logoTextPlain}>the</div>
         <div className={styles.logoTextGif}>trav</div>
